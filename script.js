1c5c46f1b4863a7a6c253a37bb77967cf3d60ac5
Rajkirantagde <121598976+rajutagde@users.noreply.github.com>
2023-04-23 19:41:23 +0530

    created a submission

--- script.js
+++
@@ -1,7 +0,0 @@
-function getFormvalue() {
-  const form = document.getElementById("form1");
-  const fname = form.elements["fname"].value;
-  const lname = form.elements["lname"].value;
-  alert(`First Name: ${fname}\nLast Name: ${lname}`);
-}
-
